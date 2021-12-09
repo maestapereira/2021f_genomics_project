524b20cf6852f8ed7caf387e9081a7d4f7a7c01b
maestapereira <91157159+maestapereira@users.noreply.github.com>
2021-12-08 19:08:35 -0500

    Added label data

--- data_conversion.py
+++ data_conversion.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # import libraries
 import pandas as pd
 import os
@@ -8,16 +5,23 @@ import os
 # raw data in portuguese
 url_data = 'https://github.com/alura-cursos/imersaodados3/blob/main/dados/dados_experimentos.zip?raw=true'
 # make data into df, decompressing it
-df_raw = pd.read_csv(url_data, compression = 'zip')
-
+df_feat = pd.read_csv(url_data, compression = 'zip')
 # create df and traslate data columns
-df_english = df_raw.rename(columns={'tratamento':'with_drug', 'tempo':'time', 'dose':'dosage', 'droga':'drug'})
+df_feat = df_feat.rename(columns={'tratamento':'with_drug', 'tempo':'time', 'dose':'dosage', 'droga':'drug'})
 # make drug column into boolean, True is for drug, False if for control
-df_english.with_drug = df_english.with_drug=='com_droga'
-
+df_feat.with_drug = df_feat.with_drug=='com_droga'
 # cd where df will be saved
 os.chdir('genomics_project\\genomics_project')
 os.getcwd()
+# save features in a zip file
+df_feat.to_csv('feature_data.zip', compression='zip')
 
-# save df
-df_english.to_csv('raw_data.zip', compression='zip')
+# save labels in a zip file
+df_labels = pd.read_csv('https://github.com/alura-cursos/imersaodados3/blob/main/dados/dados_resultados.csv?raw=true')
+df_labels.head()
+df_labels.to_csv('label_data.zip', compression='zip')
+
+# save features and labels in a single zip file
+df_raw = pd.merge(df_feat, df_labels, on='id')
+df_raw['n_moa'] = df_labels.drop('id', axis=1).sum(axis=1)
+df_labels.to_csv('raw_data.zip', compression='zip')
